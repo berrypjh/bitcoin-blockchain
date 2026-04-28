4b7b6ad340e85c278d509484e32b76dd4d35b2aa
Junhyuk Park <junhyeg08@gmail.com>
2026-04-28 20:18:16 +0900

    refactor(client): sidebar 폴더 재편
    - sidebar 폴더 내부 코드 복구

---
+++ apps/client/src/layout/sidebar/MenuList.jsx
@@ -0,0 +1,19 @@
+import { Typography } from '@mui/material';
+
+import NavGroup from './NavGroup';
+import menuItem from '../../menu-items';
+
+const MenuList = () => {
+  const navItems = menuItem.items.map((item) => {
+    if (item.type === 'group') return <NavGroup key={item.id} item={item} />;
+    return (
+      <Typography key={item.id} variant="h6" color="error" align="center">
+        Menu Items Error
+      </Typography>
+    );
+  });
+
+  return <>{navItems}</>;
+};
+
+export default MenuList;
---
+++ apps/client/src/layout/sidebar/NavItem.jsx
@@ -0,0 +1,44 @@
+import { forwardRef } from 'react';
+import { Link } from 'react-router-dom';
+import { ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+
+const NavItem = ({ item, level }) => {
+  const Icon = item.icon;
+  const itemIcon = item?.icon ? <Icon stroke={1.5} size="1.3rem" /> : <FiberManualRecordIcon />;
+  const itemTarget = item.target ? '_blank' : '_self';
+
+  const listItemProps = item?.external
+    ? { component: 'a', href: item.url, target: itemTarget }
+    : {
+        component: forwardRef((props, ref) => (
+          <Link ref={ref} {...props} to={item.url} target={itemTarget} />
+        )),
+      };
+
+  return (
+    <ListItemButton
+      {...listItemProps}
+      disabled={item.disabled}
+      sx={{
+        mb: 0.5,
+        alignItems: 'flex-start',
+        backgroundColor: level > 1 ? 'transparent !important' : 'inherit',
+        py: level > 1 ? 1 : 1.25,
+        pl: `${level * 24}px`,
+        display: 'inline-block',
+        marginLeft: '-10px',
+      }}
+    >
+      <ListItemIcon>
+        {itemIcon}
+        <ListItemText
+          sx={{ pl: `${level * 4}px`, pr: `${level * 8}px` }}
+          primary={<Typography variant="h5" color="inherit">{item.title}</Typography>}
+        />
+      </ListItemIcon>
+    </ListItemButton>
+  );
+};
+
+export default NavItem;
